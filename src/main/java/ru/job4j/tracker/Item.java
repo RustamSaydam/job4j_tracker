2cee9f1a103c6dbc12d40f5e5e622f9d2cde2bc1
RustamSaydam <rustamsaydam@icloud.com>
2022-06-20 16:36:27 +0300

    4. Сравнение моделей. Метод equals

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -20,6 +20,10 @@ public class Item {
         this.name = name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Item(LocalDateTime created) {
         this.created = created;
     }
