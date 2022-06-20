6bf34dabee87ed0f4680cd7849094bbb620c6494
RustamSaydam <rustamsaydam@icloud.com>
2022-06-20 11:28:22 +0300

    Просто трекер

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,14 +5,15 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class TrackerTest {
-    @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
-    }
+    public class TrackerTest {
+
+        @Test
+        public void whenAddNewItemThenTrackerHasSameItem() {
+            Tracker tracker = new Tracker();
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName(), is(item.getName()));
+        }
 }
