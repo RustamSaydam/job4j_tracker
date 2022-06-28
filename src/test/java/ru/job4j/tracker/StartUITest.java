75fa0baf5e23fe6af35b763b90c1f541d40b3286
RustamSaydam <rustamsaydam@icloud.com>
2022-06-28 15:58:06 +0300

    5. Input и полиморфизм

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,19 +1,32 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
 
-        @Test
-        public void whenAddItem () {
-            String[] answers = {"Fix PC"};
-            Input input = new StubInput(answers);
-            Tracker tracker = new Tracker();
-            StartUI.editItem(input, tracker);
-            Item created = tracker.findAll()[0];
-            Item expected = new Item("Fix PC");
-            assertThat(created.getName(), is(expected.getName()));
-        }
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.addNewItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void createItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("New Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()),"edit item"};
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edit item"));
+    }
+}
