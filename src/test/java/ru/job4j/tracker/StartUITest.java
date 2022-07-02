15ed4c4318f8c60dc7bfaa016836586346792361
RustamSaydam <rustamsaydam@icloud.com>
2022-07-02 18:11:43 +0300

    8. Реализация меню за счет шаблона стратегия.(TEST DELETE

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -9,47 +9,5 @@ import org.hamcrest.Matchers;
 
 public class StartUITest {
 
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.addNewItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenEditItem() {
-        String[] answers = {"Test Item"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.addNewItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Test Item");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void createItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("New Item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()), "edit item"};
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName(), is("edit item"));
-    }
-
-    @Test
-    public void deleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("New Item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item delete = tracker.findById(item.getId());
-        assertThat(delete, Matchers.is(nullValue()));
-    }
 }
+
