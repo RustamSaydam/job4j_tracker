3021192ab088e4bde0b65edbe2f72bc853c0f810
RustamSaydam <rustamsaydam@icloud.com>
2022-07-04 10:33:59 +0300

    8. Реализация меню за счет шаблона стратегия.(Exit FIX

--- src/main/java/ru/job4j/tracker/Exit.java
+++ src/main/java/ru/job4j/tracker/Exit.java
@@ -8,13 +8,6 @@ public class Exit implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Delete item ===");
-        int id = input.askInt("Enter id ");
-        if (tracker.delete(id)) {
-            System.out.println("Item with ID " + id + " delete");
-        } else {
-            System.out.println("There are no items with such id!");
-        }
         return false;
     }
 }
