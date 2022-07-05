56c93df0402e84202576d1d6fcfb965e6ecd11ae
RustamSaydam <rustamsaydam@icloud.com>
2022-07-05 16:56:50 +0300

    9.1 Зависимость от System.out (FIX)

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -18,7 +18,7 @@ public class CreateAction implements UserAction {
         String name = input.askStr("Enter name");
         Item item = new Item(name);
         tracker.add(item);
-        System.out.println("Add item: " + item);
+        out.println("Add item: " + item);
         return true;
     }
 }
--- src/main/java/ru/job4j/tracker/EditAction.java
+++ src/main/java/ru/job4j/tracker/EditAction.java
@@ -14,14 +14,14 @@ public class EditAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("=== Edit item ==");
+        out.println("=== Edit item ==");
         int id = input.askInt("press id of item you want to replace: ");
         String name = input.askStr("press name of new item: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
-            System.out.println("Item with ID " + id + " edited");
+            out.println("Item with ID " + id + " edited");
         } else {
-            System.out.println("Item with ID " + id + " is not edited");
+            out.println("Item with ID " + id + " is not edited");
         }
         return true;
     }
