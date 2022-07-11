cb97cf284ab0e241712f4ec850133f499dcf6b44
RustamSaydam <rustamsaydam@icloud.com>
2022-07-11 13:30:03 +0300

    0.3. Тестирование исключений с junit

--- src/test/java/ru/job4j/ex/FindMaxLenghtTest.java
+++ src/test/java/ru/job4j/ex/FindMaxLenghtTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
 public class FindMaxLenghtTest {
-
     @Test(expected = IllegalArgumentException.class)
     public void whenFindMaxLength() {
         Fact fact = new Fact();
