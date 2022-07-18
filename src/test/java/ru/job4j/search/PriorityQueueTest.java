20c89be9bbd167e0fa69d8a781ce89d3ba9e47aa
RustamSaydam <rustamsaydam@icloud.com>
2022-07-18 15:48:21 +0300

    2. Очередь с приоритетом на LinkedList [

---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PriorityQueueTest {
+
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("Enemy", 5));
+        queue.put(new Task("Hero", 1));
+        queue.put(new Task("Low", 2));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("Hero"));
+    }
+}
+
+
