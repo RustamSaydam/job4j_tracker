fc30e2ebbbd6b4bb3c1709200a2e2fd726372525
RustamSaydam <rustamsaydam@icloud.com>
2022-07-21 17:50:40 +0300

    2. Идентичные тексты

---
+++ src/test/java/ru/job4j/collection/UniqueTextTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class UniqueTextTest {
+
+   @Test
+    public void isEqualsTrue() {
+       String origin = "My cat eats a mouse and milk";
+       String text =  "My cat eats milk and a mouse";
+       assertThat(UniqueText.isEquals(origin, text), is(true));
+   }
+
+   @Test
+    public void isEqualsFalse() {
+       String origin = "My cat eats a mouse";
+       String text = "A mouse is eaten by a cat";
+       assertThat(UniqueText.isEquals(origin, text), is(false));
+   }
+
+}
