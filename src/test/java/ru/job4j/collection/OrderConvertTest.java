1fadae1343e2135e1288231e57c927d5bf4f4919
RustamSaydam <rustamsaydam@icloud.com>
2022-07-23 13:23:31 +0300

    2. Преобразования List в Map. Test

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -18,4 +18,18 @@ public class OrderConvertTest {
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3464"), is(new Order("3464", "Russ")));
     }
+
+    @Test
+    public void whenDuplicateTest() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3464", "Russ"));
+        orders.add(new Order("3464", "Russ"));
+        orders.add(new Order("3234", "Rustam"));
+        orders.add(new Order("3214", "Rustam"));
+        orders.add(new Order("3224", "Rustam"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        int expectedSize = 4; //   проверяет количество одинаковых названий (тут их 4) если добавить строку с друг именем то будет 5
+        int outSize = map.size();
+        assertEquals(expectedSize, outSize);
+    }
 }
