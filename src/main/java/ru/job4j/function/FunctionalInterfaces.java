b076b6a780a6b27c58b7b433dc2f8f300e2dd823
RustamSaydam <rustamsaydam@icloud.com>
2022-07-30 18:44:50 +0300

    2.2. Функциональный интерфейс

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -7,7 +7,7 @@ import java.util.function.*;
 public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-        BiConsumer<Integer, String> biCon = (s, s1) -> map.put(s, s1);
+        BiConsumer<Integer, String> biCon = (i, s) -> map.put(i, s);
         biCon.accept(1, "one");
         biCon.accept(2, "two");
         biCon.accept(3, "three");
@@ -16,7 +16,7 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (s, s1) -> s % 2 == 0 || s1.length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer s : map.keySet()) {
             if (biPred.test(s, map.get(s))) {
                 System.out.println("key: " + s + "value: " + map.get(s));
