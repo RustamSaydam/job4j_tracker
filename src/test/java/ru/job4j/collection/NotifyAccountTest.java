3563b120f34defaabd3bcfcf3cfbe2c8296df101
RustamSaydam <rustamsaydam@icloud.com>
2022-08-05 17:10:54 +0300

    1. Метод .of

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -15,12 +15,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                new Account("123", "Rustam Saydam", "eDer343ft"),
                 new Account("142", "Rustam Saydam", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Rustam Saydam", "eDer343ft"),
                         new Account("142", "Rustam Saydam", "000001")
                 )
@@ -30,13 +30,13 @@ public class NotifyAccountTest {
 
     @Test
     public void whenHasEqualsAccountThenDeleteClone() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
